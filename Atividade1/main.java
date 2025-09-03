5365bec1e0d8c3d497ee03c3e0bd21b73e886f82
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-09-03 14:25:31 -0300

    add

--- Atividade1/main.java
+++ Atividade1/main.java
@@ -1,3 +1,11 @@
-public class Main {
+public class main {
+
+    public static void main(String[] args) {
+        
+        Programa Skyrim = new Programa(8, 50, 1000);
+        Programa COD_WAR_ZONE = new Programa(8, 5000, 1000);
+        Programa GTA_VI = new Programa(64, 400, 1000);
+        Computador computador = new Computador(32, 1000, 5, 10000f);
+    }
     
 }

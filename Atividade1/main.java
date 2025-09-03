f5d57e3361f3b6aebd4b17037e1bc44663d0437b
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-09-03 14:40:12 -0300

    add

--- Atividade1/main.java
+++ Atividade1/main.java
@@ -6,6 +6,21 @@ public class main {
         Programa COD_WAR_ZONE = new Programa(8, 5000, 1000);
         Programa GTA_VI = new Programa(64, 400, 1000);
         Computador computador = new Computador(32, 1000, 5, 10000f);
+
+        public void executarPrograma(Programa p){
+            if(computador.getmemoriaRam() >= p.getMemoriaRamAlocada() && computador.getSDD() >= p.getSSDocupado()){
+                System.out.println("Programa execultado com sucesso");
+            }
+            if(computador.getmemoriaRam() < getMemoriaRamAlocada()){
+                System.out.println("Memoria RAM insuficiente para execultar o proframa");
+            }
+            if (computador.getSDD() < getSSDocupado()) {
+
+                System.out.println("Espaço em disco insuficiente para execitar o programa");
+                
+            }
+
+        }
     }
-    
+
 }
